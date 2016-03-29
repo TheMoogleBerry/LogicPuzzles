e025e0259dc439f4684929bd9be94a392661326e
rosensts <rosenstein.18@osu.edu>
2016-03-29 11:51:02 -0400

    Updated class to include import hint details

--- extract_hint_info.py
+++ extract_hint_info.py
@@ -1,4 +1,4 @@
-from nltk.corpus import stopwords
+import string
 
 '''
 For this logic puzzel there are three sections
@@ -6,27 +6,48 @@ A. Name
 B. Departure Date
 C. Location
 
-- The first step is to decide which section a given clue applies to
+- The first step is to decide which properties a given clue applies to
+- Next, the implications must be determined using keywords (not, after, or, either...)
 '''
 
 class Hint(object):
 
+	'''
+	We need to figure out what the appropriate variables are for each logic puzzel.
+	Perhaps this can be done with the POS classifier
+	'''
 	stopper = ['is', 'to', 'on']
-	names = ['Luke', 'Allen', 'Chris', 'Julio']
-	dates = ['10', '11', '12', '13']
-	locations = ['Buttonwillow', 'Coulterville', 'Farley', 'Leland']
+	keywords = ['either', 'not', 'or', 'after', 'day']
 
-	def __init__(self, hint_text):
-		self.text = hint_text
+	# Lists of all properties in the given logic puzzel
+	names = ['luke', 'allen', 'chris', 'julio']
+	dates = ['10', '11', '12', '13']
+	locations = ['buttonwillow', 'coulterville', 'farley', 'leland']
+
+	def __init__(self, hint_text): # property_array
+		'''
+		Eventually include an array of arrays that contain all properties of the given logic puzzel
+		as a required initialization parameter.
+		For this puzzel, the following would be the property_array:
+		[ ['luke', 'allen', 'chris', 'julio'],
+		['10', '11', '12', '13'],
+		['buttonwillow', 'coulterville', 'farley', 'leland'] ]
+		'''
+		self.text = hint_text.lower()
+		# self.property_array = property_array
 		self.sections = dict()
 
 		self.process_hint(hint_text)
 
 	def process_hint(self, hint_text):
-		new_hint = [i for i in self.text.split() if i not in self.stopper]
+		# remove punctuation
+		exclude = set(string.punctuation)
+		s = ''.join(ch for ch in self.text if ch not in exclude)
+		new_hint = [i for i in s.split() if i not in self.stopper]
 		self.sections['names'] = list()
 		self.sections['dates'] = list()
 		self.sections['locations'] = list()
+		self.sections['keywords'] = list()
 		for n in new_hint:
 			if n in self.names:
 				self.sections['names'].append(n)
@@ -34,8 +55,25 @@ class Hint(object):
 				self.sections['dates'].append(n)
 			if n in self.locations:
 				self.sections['locations'].append(n)
+			if n in self.keywords or ( n.isdigit() and n not in self.dates):
+				self.sections['keywords'].append(n)
 
-hint = Hint("Luke is scheduled to leave on June 11")
-print(hint.sections['names'])
-print(hint.sections['dates'])
-print(hint.sections['locations'])
+	def print_hint(self):
+		print("original text: ", self.text)
+		print(self.sections['names'])
+		print(self.sections['dates'])
+		print(self.sections['locations'])
+		print(self.sections['keywords'])
+
+hint1 = Hint('The person departing from Buttonwillow will leave 2 days after Julio.')
+hint2 = Hint('The person working on June 11 is either the person departing from Coulterville or Allen.')
+hint3 = Hint('The conductor departing from Leland will leave 1 day after Chris')
+hint4 = Hint('Luke is scheduled to leave on June 11.')
+
+hints = [hint1, hint2, hint3, hint4]
+for hint in hints:
+	hint.print_hint()
+
+
+
+
